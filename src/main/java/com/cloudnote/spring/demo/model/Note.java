2a0380d3afa27132f6bc4ecec0dee0c1a8400a32
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-03-24 20:32:31 +0530

    set up

---
+++ src/main/java/com/cloudnote/spring/demo/model/Note.java
@@ -0,0 +1,19 @@
+package com.cloudnote.spring.demo.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String content;
+
+    private String ownerUsername;
+}
