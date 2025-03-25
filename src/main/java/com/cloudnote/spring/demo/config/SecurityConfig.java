1e96c168c34de5e0dc05a29d8c960280132b901e
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-03-25 17:16:22 +0530

    moving towards role based auth

--- src/main/java/com/cloudnote/spring/demo/config/SecurityConfig.java
+++ src/main/java/com/cloudnote/spring/demo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,20 +15,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin").denyAll()
-                        .requestMatchers("/admin/**").denyAll()
-                        .anyRequest().authenticated());
-        // http.formLogin(withDefaults());
-        http.csrf(csrf->csrf.disable());
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
---
+++ src/main/java/com/cloudnote/spring/demo/model/AppRole.java
@@ -0,0 +1,11 @@
+package com.cloudnote.spring.demo.model;
+
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
+
+
+
+
