ee694030a9599892189f3e2575cb1c068409706c
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-03-27 19:15:05 +0530

    logging audits

--- src/main/java/com/cloudnote/spring/demo/service/impl/NoteServiceImpl.java
+++ src/main/java/com/cloudnote/spring/demo/service/impl/NoteServiceImpl.java
@@ -6,6 +6,7 @@ package com.cloudnote.spring.demo.service.impl;
 import com.cloudnote.spring.demo.Repository.NoteRepository;
 
 import com.cloudnote.spring.demo.model.Note;
+import com.cloudnote.spring.demo.service.AuditService;
 import com.cloudnote.spring.demo.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,8 @@ public class NoteServiceImpl implements NoteService {
     private NoteRepository noteRepository;
 
 
+    @Autowired
+    AuditService auditService;
 
     @Override
     public Note createNoteForUser(String username, String content) {
@@ -26,7 +29,7 @@ public class NoteServiceImpl implements NoteService {
         note.setContent(content);
         note.setOwnerUsername(username);
         Note savedNote = noteRepository.save(note);
-
+        auditService.logNoteCreation(username,note);
         return savedNote;
     }
 
@@ -35,7 +38,7 @@ public class NoteServiceImpl implements NoteService {
         Note note = noteRepository.findById(noteId).orElseThrow(()
                 -> new RuntimeException("Note not found"));
         note.setContent(content);
-
+          auditService.logNoteUpdate(username,note);
         return noteRepository.save(note);
     }
 
@@ -46,10 +49,12 @@ public class NoteServiceImpl implements NoteService {
         );
 
         noteRepository.delete(note);
+        auditService.logNoteDeletion(username,noteId);
     }
 
     @Override
     public List<Note> getNotesForUser(String username) {
+
         return noteRepository
                 .findByOwnerUsername(username);
     }
