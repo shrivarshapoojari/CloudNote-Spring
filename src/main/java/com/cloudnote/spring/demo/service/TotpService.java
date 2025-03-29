2b633f78b5c526ac552bb9b0ef98a6e9700778af
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-03-29 09:26:40 +0530

    implementing TOTP service for MFA

---
+++ src/main/java/com/cloudnote/spring/demo/service/TotpService.java
@@ -0,0 +1,12 @@
+package com.cloudnote.spring.demo.service;
+
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+
+public interface TotpService {
+
+    GoogleAuthenticatorKey generateSecret();
+
+    String getQrCodeUrl(GoogleAuthenticatorKey secret, String username);
+
+    boolean verifyCode(String secret, int code);
+}
---
+++ src/main/java/com/cloudnote/spring/demo/service/impl/TotpServiceImpl.java
@@ -0,0 +1,40 @@
+package com.cloudnote.spring.demo.service.impl;
+
+import com.cloudnote.spring.demo.service.TotpService;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TotpServiceImpl implements TotpService {
+
+
+    private final GoogleAuthenticator gAuth;
+
+    public TotpServiceImpl(GoogleAuthenticator gAuth)
+    {
+        this.gAuth=gAuth;
+    }
+
+    public TotpServiceImpl()
+    {
+        this.gAuth=new GoogleAuthenticator();
+    }
+    @Override
+    public GoogleAuthenticatorKey generateSecret()
+    {
+        return gAuth.createCredentials();
+    }
+    @Override
+    public String getQrCodeUrl(GoogleAuthenticatorKey secret, String username)
+    {
+        return GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("CloudNotes",username,secret);
+    }
+
+    @Override
+    public boolean verifyCode(String secret, int code)
+    {
+         return  gAuth.authorize(secret,code);
+    }
+}
